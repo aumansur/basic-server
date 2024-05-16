40abdd66881cd8c87a5b268f5d125103e901694c
aumansur <ahammadullah2001@gmail.com>
2024-05-16 19:29:47 +0600

    project close

--- dist/app.js
+++ dist/app.js
@@ -13,6 +13,28 @@ const logger = (req, res, next) => {
     console.log(req.url, req.method, req.hostname);
     next();
 };
+const userRouter = express_1.default.Router();
+const courseRouter = express_1.default.Router();
+app.use("/api/v1/courses", courseRouter);
+courseRouter.post("/create-course", (req, res) => {
+    const course = req.body;
+    console.log(course);
+    res.json({
+        success: true,
+        message: "course created successfully",
+        data: course,
+    });
+});
+app.use("/api/v1/users", userRouter);
+userRouter.get("/create-user", (req, res) => {
+    const user = req.body;
+    console.log(user);
+    res.json({
+        success: true,
+        message: "user created successfully",
+        data: user,
+    });
+});
 app.get("/", logger, (req, res) => {
     res.send("Hello Developer mansur");
 });
--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { NextFunction, Request, Response } from "express";
+import express, { NextFunction, Request, Response, Router } from "express";
 const app = express();
 const port = 3000;
 
@@ -10,6 +10,29 @@ const logger = (req: Request, res: Response, next: NextFunction) => {
   console.log(req.url, req.method, req.hostname);
   next();
 };
+
+const userRouter = express.Router();
+const courseRouter = express.Router();
+app.use("/api/v1/courses", courseRouter);
+courseRouter.post("/create-course", (req: Request, res: Response) => {
+  const course = req.body;
+  console.log(course);
+  res.json({
+    success: true,
+    message: "course created successfully",
+    data: course,
+  });
+});
+app.use("/api/v1/users", userRouter);
+userRouter.get("/create-user", (req: Request, res: Response) => {
+  const user = req.body;
+  console.log(user);
+  res.json({
+    success: true,
+    message: "user created successfully",
+    data: user,
+  });
+});
 
 app.get("/", logger, (req: Request, res: Response) => {
   res.send("Hello Developer mansur");
